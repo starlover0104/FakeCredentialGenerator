c66686018875d76c68155d0313b7eb4ef22434fa
Leah <git.sixfold763@passmail.net>
2024-12-05 11:14:25 -0500

    remove unneeded imports and also print output to terminal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-import os
+import csv, random
 from faker import Faker
-from datetime import datetime, timedelta
-import random
 
 print("made by starlover")
 
@@ -14,18 +11,25 @@ def generate_csv():
         fieldnames = ['Firstname', 'Lastname', 'Password', 'Birthday', 'Username']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
+        print("\nGenerated User Data:")
+        print("-" * 80)
         for i in range(20):
             firstname = fake.first_name()
             lastname = fake.last_name()
             password = fake.password()
             birthday = fake.date_of_birth().strftime('%m/%d/%Y')
             username = f"{fake.first_name().lower()}{fake.last_name().lower()}{random.randint(1, 100)}@{fake.domain_name()}"
-            writer.writerow({
+            data = {
                 'Firstname': firstname,
                 'Lastname': lastname,
                 'Password': password,
                 'Birthday': birthday,
                 'Username': username
-            })
+            }
+            writer.writerow(data)
+            print(f"User {i+1}:")
+            for key, value in data.items():
+                print(f"{key}: {value}")
+            print("-" * 80)
 
 generate_csv()
